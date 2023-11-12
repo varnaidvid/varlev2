afd253237cc57e2bda1417ba33db2462cd172c80
vebesz <barnabas.varszegi@gmail.com>
2023-11-12 21:13:42 +0100

    better game ui

--- components/verseny/wordCard.tsx
+++ components/verseny/wordCard.tsx
@@ -1,6 +1,6 @@
 export default function WordCard({ word }: { word: string }) {
   return (
-    <div className="font-mono bg-gray-200 p-4 rounded-lg flex items-center justify-center text-semibold text-base">
+    <div className="font-mono border-dashed border-2 border-gray-400 p-4 rounded-lg flex items-center justify-center text-semibold text-lg capitalize font-bold text-gray-600">
       {word}
     </div>
   );
